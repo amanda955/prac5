6ec47e406d3258cd7a12fc0d02eb10e3e01a3a53
amanda955 <amanda.jayasingheappuhamilage@student.adelaide.edu.au>
2025-09-25 00:46:18 +0930

    Update Mult.asm

--- Mult/Mult.asm
+++ Mult/Mult.asm
@@ -9,77 +9,121 @@
 
 // Put your code here.
 @R0
-M=0
+M=0              //result = 0
+
 
 @R1
 D=M
-@AVAL
-M=D         //AVAL = R1
-
+@A
+M=D              //A = R1
 @R2
 D=M
-@BVAL
-M=D  
+@B
+M=D            //B = R2
+
+//Recording sign flags and make A = |R1| and B = |R2|
+@0
+D=A
+@SA
+M=D                //SA = 0 (R1 non-negative by default)
+@A
+D=M
+@A_NONNEG
+D;JGE             //if A >= 0 skip
+@1
+D=A
+@SA
+M=D                //SA = 1 (R1 was negative)
+@A
+M=-M              //A = |A|
+(A_NONNEG)
 
 @0
 D=A
-@SIGN
-M=D 
-
-@AVAL
+@SB
+M=D                //SB = 0 
+@B
 D=M
-@CHECK_B
-D;JGE 
+@B_NONNEG
+D;JGE               //skip if B >= 0 
+@1
+D=A
+@SB
+M=D                //SB = 1 (R2 was negative)
+@B
+M=-M              //B = |B|
+(B_NONNEG)
 
-@SIGN
-M=M+1         
-@AVAL
-M=-M 
-
-@BVAL
+@SA
 D=M
-@SET_COUNTER
-D;JGE 
+@SB
+D=D+M
+@NEG
+M=D                
 
-@SIGN
-M=M+1         
-@BVAL
-M=-M           
+// Choose smaller abs as COUNT , larger as ADD 
+@A
+D=M
+@B
+D=D-M              //D = A - B
+@A_LE_B
+D;JLE              //use A as COUNT if A <= B
 
-(SET_COUNTER)
-@BVAL
+//Case A > B: COUNT=B, ADD=A
+@B
 D=M
 @COUNT
 M=D
-
-(LOOP)
-    @COUNT
-    D=M
-    @END
-    D;JEQ      
-
-    //result is  result + AVAL
-    @AVAL
-    D=M
-    @R0
-    M=M+D
-
-    //COUNT = COUNT - 1
-    @COUNT
-    M=M-1
-
-    @LOOP
-    0;JMP
-
-(END)
-@SIGN
+@A
 D=M
-@POSITIVE
-D;JEQ
+@ADD
+M=D
+@LOOP_START
+0;JMP
 
+//Case A <= B: COUNT=A, ADD=B
+(A_LE_B)
+@A
+D=M
+@COUNT
+M=D
+@B
+D=M
+@ADD
+M=D
+
+//Main loop
+(LOOP_START)
+@COUNT
+D=M
+@APPLY_SIGN
+D;JEQ              //Stop adding if COUNT==0
+
+@ADD
+D=M
+@R0
+M=M+D             //R0 += ADD
+
+@COUNT
+M=M-1              //COUNT
+@LOOP_START
+0;JMP
+
+(APPLY_SIGN)
+@NEG
+D=M
+D=D-1              //D = NEG - 1
+@MAKE_NEG
+D;JEQ            
+@FIN
+0;JMP
+
+(MAKE_NEG)
 @R0
 M=-M
+@FIN
+0;JMP
 
-(POSITIVE)
-@END
+(FIN)
+@FIN
 0;JMP
